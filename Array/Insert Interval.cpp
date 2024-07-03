fb44ed73fa8a8cd9d4403d19a2982e31d583b25c
Juhi Pathak <73741643+JuhiPathak23@users.noreply.github.com>
2024-07-03 13:50:23 +0530

    Create Insert Interval.cpp

---
+++ Array/Insert Interval.cpp
@@ -0,0 +1,25 @@
+//Q: https://www.geeksforgeeks.org/insert-in-sorted-and-non-overlapping-interval-array/
+
+//Solution:
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back({newInterval});
+        sort(intervals.begin(),intervals.end());
+        int s=0;
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[s][1]>=intervals[i][0])
+                intervals[s][1]=max(intervals[s][1],intervals[i][1]);
+            else{
+                s++;
+                intervals[s]=intervals[i];
+            }
+        }
+        vector<vector<int>>res;
+        for(int k=0;k<=s;k++)   res.push_back(intervals[k]);
+        return res;
+    }
+};
+
+//TC: O(N* log N), SC: O(1)
